a6857d4e0dbadc755488e8dce4a86f3551de111f
Nikolay Tsvetkov <nic.tsvetcov@gmail.com>
2021-09-05 17:31:34 +0300

    end app

--- app/src/main/java/tns/fit/bstu/by/MainActivity.java
+++ app/src/main/java/tns/fit/bstu/by/MainActivity.java
@@ -3,6 +3,12 @@ package tns.fit.bstu.by;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import tns.fit.bstu.by.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 10; i++) {
+            Log.d("MainActivity", " counter =" + i);
+
+        }
+        extracted();
+    }
+
+    private void extracted() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/tns/fit/bstu/by/text/TextFunction.java
@@ -0,0 +1,7 @@
+package tns.fit.bstu.by.text;
+
+public class TextFunction {
+    public String getValue(){
+        return "Hello from first project i'am Nikolay";
+    }
+}
